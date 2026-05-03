d84613ed9bd511788ae6c345414cb4c7f2c0fc79
Theodore Bjernhed <uik1475@edu.kunskapsskolan.se>
2026-05-03 15:09:26 +0200

    chore: make editor show up

--- scripts/commit_tests_pass.bash
+++ scripts/commit_tests_pass.bash
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-watchexec --clear --shell bash -- 'make test && if ! git diff --quiet; then git commit -a; else echo "No uncommitted changes to commit."; fi'
+watchexec --clear --shell bash -- 'make test && if ! git diff --quiet; then kitty -d . -e git commit -a; else echo "No uncommitted changes to commit."; fi'
